7eb79d46ff6803a72b65f22d466c4687ced55e44
Kevin <webdevbox91@gmail.com>
2022-05-23 16:47:48 +0200

    fix: factorisation

--- script/script2.js
+++ script/script2.js
@@ -1,103 +1,106 @@
 const weatherIcons = {
-    "Rain":"wi wi-day-rain",
-    "Clouds":"wi wi-day-cloudy",
-    "Clear" : "wi wi-day-sunny",
-    "Snow" : "wi wi-day-snow",
-    "Mist" : "wi wi-day-fog",
-    "Drizzle" :"wi wi-day-sleet"
-}
+  Rain: "wi wi-day-rain",
+  Clouds: "wi wi-day-cloudy",
+  Clear: "wi wi-day-sunny",
+  Snow: "wi wi-day-snow",
+  Mist: "wi wi-day-fog",
+  Drizzle: "wi wi-day-sleet",
+};
+const ville = document.getElementById("ville");
+const timeLocal = document.getElementById("timeLocal");
+const timeByZone = document.getElementById("timeByZone");
+const temperature = document.getElementById("temperature");
+const descriptions = document.getElementById("descriptions");
+const icon = document.querySelector("i.wi");
+const blocpage = document.getElementById("blocpage");
+const humidity = document.getElementById("humidity");
+const coord = document.getElementById("coord");
+const pression = document.getElementById("pression");
 
-const myConvertH= (strD) => {
-  return new Date(strD).getHours()+"h" + new Date(strD).getMinutes() +"m" + new Date(strD).getSeconds()+"s";
-}
-const myConvertM= (strD) => {
-  return new Date(strD).getHours()+"h" + new Date(strD).getMinutes() +"m";
-}
+const myConvertH = (strD, withIp) => {
+  let date = new Date(strD);
+  const hour = date.getHours() <= 9 ? "0" + date.getHours() : date.getHours();
+  const min =
+    date.getMinutes() <= 9 ? "0" + date.getMinutes() : date.getMinutes();
+  const sec =
+    date.getSeconds() <= 9 ? "0" + date.getSeconds() : date.getSeconds();
 
-const main = async(withIp = true) => {
-    let ville;
+  const horaire = hour + "h" + min + "m" + sec + "s";
 
-    if(withIp){
-        const ip =  await fetch(`https://api.ipify.org?format=json`)
-        .then((result) => result.json())
-        .then((json) =>  json.ip);
-
-     ville = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_xoG8JFk0Tfpf1hlgzIvXLGep5S4Tn&ipAddress=${ip}`)
-        .then((result) => result.json())
-        .then((json) => json.location.city);
-
-    }else{
-        ville = document.getElementById('ville').textContent;
-    }
-         
-    const meteo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=fd0473cb1f0e58f3f02b2e341c459e78&lang=fr&units=metric`)
-        .then((result) => result.json())
-        .then((json) => json);
-
-        
-
-
-        setTimeout(weatherInfo(meteo),1000)
-}
-
-const weatherInfo = async (data)=>{
-    const name = data.name
-    const temperature = Math.round(data.main.temp)
-    const conditions = data.weather[0].main
-    const descriptions = data.weather[0].description
-    const latitude = data.coord.lat;
-    const longitude = data.coord.lon;
-    const vent = data.wind.speed
-    const humid = data.main.humidity
-    const press = data.main.pressure
-    
-    const heureL = await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=NETFNVYNIQGM&format=json&by=position&lat=${latitude}&lng=${longitude}`)
-        .then(result => result.json())
-        .then (json => myConvertM(json.formatted));
-
-    document.getElementById('ville').textContent = name;
-    document.getElementById('temperature').textContent = temperature;
-    document.getElementById('descriptions').textContent = descriptions;
-    document.querySelector('i.wi').className = weatherIcons[conditions]
-    document.querySelector('#blocpage').className = conditions.toLowerCase();
-    document.getElementById('humidity').textContent= `l'humidité acutelle est de ${humid}%`
-    document.getElementById('coord').textContent= `Vos coordonnées: latitude:${latitude}, longitude:${longitude}`
-    document.querySelector('#pression').textContent = `le vent est a une vitesse de ${vent}m/s et il y a une pression actuelle de ${press}hPa `
-    document.getElementById('time').textContent = `il est actuellement ${heureL} à ${name} `
-
+  withIp ? (timeByZone.textContent = horaire) : (timeLocal.textContent = horaire);
 };
 
-const affichageHeure = async function(){
-    const ip =  await fetch(`https://api.ipify.org?format=json`)
-        .then((result) => result.json())
-        .then((json) =>  json.ip);
+const getTimeByZone = (lat, long) => {
+  setInterval(() => {
+    fetch(
+      `http://api.timezonedb.com/v2.1/get-time-zone?key=NETFNVYNIQGM&format=json&by=position&lat=${lat}&lng=${long}`
+    )
+      .then((result) => result.json())
+      .then((json) => myConvertH(json.formatted));
+  }, 1000);
+};
 
-        const horaire = await fetch(`http://worldtimeapi.org/api/ip/${ip}`)
-        .then((result) => result.json())
-        .then((json) =>myConvertH(json.datetime))
-        const heure= document.getElementById('horaire')
-        heure.textContent = horaire;
+const weatherInfo = (data) => {
+  ville.textContent = data.name;
+  temperature.textContent = Math.round(data.main.temp);
+  descriptions.textContent = data.weather[0].description;
+  icon.className = weatherIcons[data.weather[0].main];
+  blocpage.className = data.weather[0].main.toLowerCase();
+  humidity.textContent = `l'humidité acutelle est de ${data.main.humidity}%`;
+  coord.textContent = `Vos coordonnées: latitude:${data.coord.latitude}, longitude:${data.coord.longitude}`;
+  pression.textContent = `le vent est a une vitesse de ${data.wind.speed}m/s et il y a une pression actuelle de ${data.main.pressure}hPa`;
+};
 
-        
- 
-    setTimeout(affichageHeure, 1000);
-}
+const weatherData = (ville, withIp) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=fd0473cb1f0e58f3f02b2e341c459e78&lang=fr&units=metric`
+  )
+    .then((result) => result.json())
+    .then((json) => {
+      weatherInfo(json);
+      !withIp && getTimeByZone(json.coord.lat, json.coord.lon);
+    });
+};
 
-const ville = document.querySelector('#ville')
+const affichageHeure = (ip) => {
+  setInterval(() => {
+    fetch(`http://worldtimeapi.org/api/ip/${ip}`)
+      .then((result) => result.json())
+      .then((json) => myConvertH(json.datetime, withIp = true));
+  }, 1000);
+};
 
-ville.addEventListener('click',()=>{
-    ville.contentEditable = true;
-})
+const getCityByIp = (ip) => {
+  fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_xoG8JFk0Tfpf1hlgzIvXLGep5S4Tn&ipAddress=${ip}`
+  )
+    .then((result) => result.json())
+    .then((json) => weatherData(json.location.city, (withIp = true)));
+};
 
-ville.addEventListener('keydown',(e)=>{
-    if(e.keyCode === 13){
-        e.preventDefault();
-        ville.contentEditable = false
-        main(false);
-    }
-})
+const main = (withIp = true) => {
+  if (withIp) {
+    fetch(`https://api.ipify.org?format=json`)
+      .then((result) => result.json())
+      .then((json) => {
+        getCityByIp(json.ip);
+        affichageHeure(json.ip);
+      });
+  } else {
+    weatherData(document.getElementById("ville").textContent);
+  }
+};
 
+ville.addEventListener("click", () => {
+  ville.contentEditable = true;
+});
 
-main()
-affichageHeure()
+ville.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    ville.contentEditable = false;
+    main((withIp = false));
+  }
+});
 
+main();
